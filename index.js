c977db4ea174513caa6bdca9f4e65710f5f06c10
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-07-18 13:18:16 +0530

    respository model setup

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express=require("express");
 const {PORT}=require('./src/config/serverConfig');
+const bodyParser = require("body-parser");
 
 const setupAndStartServer=async ()=>{
     const app=express();
+
+    app.use(bodyParser);
+    app.use(bodyParser.urlencoded({extended:true}))
 
     app.listen(PORT,()=>{
         console.log(`server is running at port ${PORT}`)
