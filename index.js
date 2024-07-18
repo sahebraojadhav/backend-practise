10612fdf6a8a83add31d6e453df2b7ea1bfcc5c7
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-07-18 11:22:59 +0530

    set up of the env varibale 1.add the variables in .env 2.import in the config 3.now import in the index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const express=require("express");
+const {PORT}=require('./src/config/serverConfig');
+
+const setupAndStartServer=async ()=>{
+    const app=express();
+
+    app.listen(PORT,()=>{
+        console.log(`server is running at port ${PORT}`)
+        console.log("hello js")
+    })
+}
+setupAndStartServer();
