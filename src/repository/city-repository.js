1d6216e2a277997cecda6e64d8d48245c41785d8
saheb <sahebraoj24pc@gmail.com>
2024-07-25 16:06:59 +0530

    Added get allcities api

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,69 +1,75 @@
-const { where } = require('sequelize');
-const {City}=require('../models/index'); 
-const e = require('express');
+const { where, Op } = require("sequelize");
+const { City } = require("../models/index");
+const e = require("express");
 
-class CityRepository{
-    async createCity({name}){
-        try{
-            const city =await City.create({name});
-            return city;
-        }
-        catch(error){
-            console.log("error creating city",error);
-            throw {error}
-        }
+class CityRepository {
+  async createCity({ name }) {
+    try {
+      const city = await City.create({ name });
+      return city;
+    } catch (error) {
+      console.log("error creating city", error);
+      throw { error };
     }
+  }
 
-    async deleteCity(cityId){
-        try{
-            await City.destroy({
-                where:{
-                    id:cityId
-                }
-            })
-        }catch(error){
-            console.log("error deleting city",error);
-            throw {error}
-        }
+  async deleteCity(cityId) {
+    try {
+      await City.destroy({
+        where: {
+          id: cityId,
+        },
+      });
+    } catch (error) {
+      console.log("error deleting city", error);
+      throw { error };
     }
+  }
 
-    async updateCity(cityId,data){
-        try{
-            const city=await City.update(data,{
-                where:{
-                    id:cityId
-                }
-            })
-            return city;
-        }
-        catch(error){
-            console.log("error occurd while deleting city");
-            throw {error};
-        }
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      console.log("error occurd while deleting city");
+      throw { error };
     }
+  }
 
-    async getCity(cityId){
-        try{
-            const city=await City.findByPk(cityId);
-            return city;
-        }catch(error){
-            console.log("Something went wrong in the repository layer")
-            throw(error);
-        }
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw error;
     }
+  }
 
-    async getAllCities(){
-        try{
-            const cities=await City.findAll();
-            return cities;
-
-        }
-        catch(error){
-            console.log("Something went wrong in the repository layer")
-            throw(error);
-        }
+  async getAllCities(filter) {
+    //filter can be empty
+    try {
+      if (filter.name) {
+        const cities = await City.findAll({
+          where: {
+            name: {
+              [Op.startsWith]: filter.name,
+            },
+          },
+        });
+        return cities;
+      }
+      const cities = await City.findAll();
+      return cities;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw error;
     }
-
+  }
 }
 
-module.exports=CityRepository;
+module.exports = CityRepository;
