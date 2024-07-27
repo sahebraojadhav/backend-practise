d0e1a4c9742a1142d823a5755493bcf0e39e46c4
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-07-27 12:59:46 +0530

    error occurd

---
+++ src/seeders/20240727055621-add-airports.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Airports',[
+      {
+        name:'kempegowda International Airport',
+        cityId:9,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },
+      {
+        name:'Mysuru Airport',
+        cityId:9,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },
+      {
+        name:'Menguluru International Airport',
+        cityId:9,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },
+      {
+        name:'Indira gandhi International Airport',
+        cityId:6,
+        createdAt:new Date(),
+        updatedAt:new Date()
+      },
+    ],{});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
